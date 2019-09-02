d984bf8e668966675f26e537b82cdc685b1af39a
crisanlucid <crisan.lucid@yahoo.com>
2019-09-03 01:15:20 +0200

    create Card layout, add Form and create submitHandler function

--- src/components/CardWeather.js
+++ src/components/CardWeather.js
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
+import { Col, Button, Form, FormGroup, Label, Input, Card, CardText, CardBody, CardTitle } from 'reactstrap';
+
 import './CardWeather.css';
 
 const keyLocalStorage = 'formWeather';
@@ -7,7 +9,31 @@ const CardWeather = () => {
 	const defaultConfig = JSON.parse(window.localStorage.getItem(keyLocalStorage)) || { city: 'Berlin' };
 	const [formState, setFormState] = useState(defaultConfig);
 	const { city } = formState;
-	return <div>MyWeather - {city}</div>;
+	const submitHandler = (e) => {
+		e.preventDefault();
+		console.log('Submit');
+	};
+	return (
+		<Fragment>
+			<Card>
+				<CardBody>
+					<CardTitle>Card Weather</CardTitle>
+					<CardText>Show weather in your city</CardText>
+					<Form onSubmit={submitHandler} method='POST'>
+						<FormGroup row>
+							<Label for='City' sm={2}>
+								City
+							</Label>
+							<Col sm={10}>
+								<Input type='city' name='ciy' id='City' placeholder='Berlin' />
+							</Col>
+						</FormGroup>
+						<Button>Submit</Button>
+					</Form>
+				</CardBody>
+			</Card>
+		</Fragment>
+	);
 };
 
 export default CardWeather;

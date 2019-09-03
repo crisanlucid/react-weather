79c94d70e7878211741f50d94cb8e18ddc4f8f1f
crisanlucid <crisan.lucid@yahoo.com>
2019-09-03 11:45:43 +0200

    update state, submit form & create submitHandler, fetch Weather API and show errors in console

--- src/components/CardWeather.js
+++ src/components/CardWeather.js
@@ -2,16 +2,45 @@ import React, { useState, Fragment } from 'react';
 import { Col, Button, Form, FormGroup, Label, Input, Card, CardText, CardBody, CardTitle } from 'reactstrap';
 
 import './CardWeather.css';
+import { forStatement } from '@babel/types';
 
 const keyLocalStorage = 'formWeather';
 
 const CardWeather = () => {
 	const defaultConfig = JSON.parse(window.localStorage.getItem(keyLocalStorage)) || { city: 'Berlin' };
 	const [formState, setFormState] = useState(defaultConfig);
+	const [error, setError] = useState('');
 	const { city } = formState;
 	const submitHandler = (e) => {
 		e.preventDefault();
-		console.log('Submit');
+		const { city } = e.target.elements;
+		setFormState({
+			...formState,
+			city: city.value,
+		});
+
+		//validation
+
+		//submit and error
+		//throw new Error(JSON.stringify(formState), null, 2);
+		fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city.value}`)
+			.then((data) => {
+				if (!data.ok) throw new Error(data.statusText);
+				console.log(data);
+				return data.json();
+			})
+			.then((data) => {
+				console.log(data);
+			})
+			.catch((e) => {
+				console.error(e);
+				setError({
+					...error,
+					error: true,
+					errorText: e,
+				});
+				// throw new Error(e, null, 2);
+			});
 	};
 	return (
 		<Fragment>
@@ -21,11 +50,11 @@ const CardWeather = () => {
 					<CardText>Show weather in your city</CardText>
 					<Form onSubmit={submitHandler} method='POST'>
 						<FormGroup row>
-							<Label for='City' sm={2}>
+							<Label for='city-input' sm={2}>
 								City
 							</Label>
 							<Col sm={10}>
-								<Input type='city' name='ciy' id='City' placeholder='Berlin' />
+								<Input name='city' id='city-input' placeholder='Berlin' />
 							</Col>
 						</FormGroup>
 						<Button>Submit</Button>
